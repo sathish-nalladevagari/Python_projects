67e2a6d3f647fd5465b2dbb23a98b8c78c0a7845
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-03-27 19:52:33 +0530

    done array and strings

---
+++ DSA/Strings/All_operations.py
@@ -0,0 +1,33 @@
+## All opertions of string
+
+class MyString:
+    def __init__(self):
+        self.string = ""
+    def length(self):
+        return len(self.string)
+    
+    def check_empty(self):
+        if len(self.string) == 0:
+            print(True)
+        else:
+            print(False)
+    
+    def concatinate(self,string1,string2):
+        print(self.string+string1+string2)
+    
+    
+    
+    def print_string(self):
+        print(self.string)
+    
+    
+
+
+
+string = MyString()
+
+
+string.concatinate("hello", "world")
+string.check_sub_string_search("hello")
+
+string.print_string()
