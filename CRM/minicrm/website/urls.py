ae8e4d88cfca1d8d438b23a7414100a8f4baf310
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-04-07 16:41:40 +0530

    completed django forms and implemented registration

--- CRM/minicrm/website/urls.py
+++ CRM/minicrm/website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index , login_user, logout_user
+from .views import index , login_user, logout_user , register_user
 
 urlpatterns = [
     path('', index ,name="home"),
     path('login/' , login_user , name='login'),
     path('logout/' , logout_user , name='logout'),
+    path('register/' , register_user , name='register'),
 ]
  
--- CRM/minicrm/website/views.py
+++ CRM/minicrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth import authenticate , login , logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -18,12 +19,34 @@ def login_user(request):
         if user is not None:
             login(request , user)
             messages.success(request, "You have been logged in")
+            redirect('home')
         else:
             messages.success(request, "You have error  logged in please try again ")
-            return redirect('login.html')
+            return redirect('login')
+    else:
+          
+        return render(request,'login.html')
     return render(request,'login.html')
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('home')
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
