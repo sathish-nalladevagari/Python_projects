6161dab72acb5925435ce3c87a5a885160c1c40d
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-04-09 08:30:34 +0530

    completed models and forms and getting todos and adding todos

--- CRUD/core/crud/urls.py
+++ CRUD/core/crud/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index , register_user , login_user
+from .views import index , register_user , login_user , logout_user , add_task
 
 urlpatterns = [
     path('', index , name='home'),
     path('register/', register_user , name='register'),
     path('login/', login_user , name='login'),
+    path('logout/', logout_user , name='logout'),
+    path('logout/', logout_user , name='logout'),
+    path('addtask/', add_task , name='add')
+    
 ]
--- CRUD/core/crud/views.py
+++ CRUD/core/crud/views.py
@@ -1,13 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .forms import RegisterForm , TodoForm
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
+from .models import Todo
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html' , {})
+	if request.user.is_authenticated:
+		todolist = Todo.objects.filter(user = request.user)
+		return render(request, 'home.html' , {'todolist':todolist})
+	return redirect('login')
+
+#Register
 
 def register_user(request):
-    return render(request, 'register.html' , {})
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username = username , password = password)
+			if user is not None:
+				login(request,user)
+				messages.success(request, "User created")
+				return redirect('home')
+	else:
+		form = RegisterForm()
+		return render(request, 'register.html' , {'form' : form})
+	return render(request, 'register.html' , {'form':form})
+               
+#Login            
 
 def login_user(request):
-    return render(request, 'login.html' , {})
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		#Authenticate
+		user = authenticate(request, username = username , password = password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "Login Success")
+			return redirect('home')
+		else:
+			messages.success(request, "Login failed")
+			return redirect('login')
+	return render(request, 'login.html')
+
+
+# Logout
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You have logged out")
+	return redirect('home')
+
+
+# Add new task
+
+def add_task(request):
+	if request.method == "POST":
+		print(request.user)
+		form = TodoForm(request.POST)
+		if form.is_valid():
+			item = form.save(commit=False)
+			item.user = request.user
+			item.save()
+
+			messages.success(request, "New Task added")
+			return redirect('home')
+	else:	
+		form = TodoForm()
+		return render(request, 'addtask.html', {'form':form})
