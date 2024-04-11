86ce88a8f72e9735f3b17f0bd935b150b5fc54aa
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-04-11 20:30:19 +0530

    initial commit for new django and react project

---
+++ CRUD_Django_and_react/core/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# Create your views here.
+
+
+class Index(APIView):
+    def get(self , request):
+        return Response({'kei' : 'hello'})
+
