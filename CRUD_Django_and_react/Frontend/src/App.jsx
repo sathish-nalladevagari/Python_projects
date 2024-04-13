6cb1b9ee2bf70045caabd9939768d6bfdff8f805
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-04-13 22:12:41 +0530

    Frontend setup and mui installation

--- CRUD_Django_and_react/Frontend/src/App.jsx
+++ CRUD_Django_and_react/Frontend/src/App.jsx
@@ -1,12 +1,28 @@
-
 import './App.css'
+import Button from '@mui/material/Button';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-      Hello world
+    <Navbar/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route  path='/register' element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+
+    </Routes>
+    
+    </BrowserRouter>
+      
+      <Button variant="contained">Hello world</Button>
     </>
   )
 }
