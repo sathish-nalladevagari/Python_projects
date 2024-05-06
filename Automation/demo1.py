b4c2d9455d65bef24d35483b3ff390fbe9947bc8
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-05-06 21:44:53 +0530

    automation

--- Automation/demo1.py
+++ Automation/demo1.py
@@ -1,29 +1,4 @@
-import mysql.connector
+def index(name):
+    return "hello" + name
 
-# Connect to the MySQL database
-try:
-    connection = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='',
-        database='employees'
-    )
-
-    if connection.is_connected():
-        print('Connected to MySQL database')
-
-    # Perform database operations here
-
-    cursor = connection.cursor()
-    cursor.execute("select * from emp_address ")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-
-    
-
-    
-except mysql.connector.Error as e:
-    print(f"Error connecting to MySQL database: {e}")
-
+print(index('sathish'))
